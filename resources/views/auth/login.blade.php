7de2a83c5c14c212607b7f1dcbd9783e666640c5
Rizki Tirta Buwono <rizkipou01@gmail.com>
2021-06-03 16:33:08 +0700

    Revisi

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -51,6 +51,13 @@
                             </div>
                         </div>
 
+
+                           <div class="text-bold">
+                            <p>Email    : admin@gmail.com</p>
+                            <p>Password : adminaja</p>
+                           </div>
+
+
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
